5a0e941330a97eee204e478416645b4779c7e764
Hussain <miralihussainkhan@gmail.com>
2017-10-11 04:58:07 +0530

    Updated to ES6 standard (source/test)

--- test/K_MeansSpec.js
+++ test/K_MeansSpec.js
@@ -1,22 +1,22 @@
-var K_Means = require('../K_Means');
-var assert = require('assert');
-var mathjs = require('mathjs');
-var sinon = require('sinon');
+let K_Means = require('../K_Means');
+let assert = require('assert');
+let mathjs = require('mathjs');
+let sinon = require('sinon');
 
 
 describe('K_Means', function() {
 
-    var callback_data;
+    let callback_data;
 
-    var callback = function(data) {
+    let callback = function(data) {
         data.clusters;
         console.log(data);
     };
 
     it("Should throw an exception if constructor parameters are not correctly set. ", function() {
-        var options = [4, 5, 10000, callback, 10];
-        var error = undefined;
-        var random_Val = Math.floor(Math.random() * 4);
+        let options = [4, 5, 10000, callback, 10];
+        let error = undefined;
+        let random_Val = Math.floor(Math.random() * 4);
         options[random_Val] = undefined; //randomly setting one of the parameters to undefined.
         try {
             new K_Means({
@@ -33,9 +33,9 @@ describe('K_Means', function() {
     });
 
     describe("While clustering centroids.", function() {
-        var data = [];
-        var data_generate = function() {
-            var i = 0,
+        let data = [];
+        let data_generate = function() {
+            let i = 0,
                 j = 0;
             for (i = 0; i < 10000; i++) {
                 data[i] = [];
@@ -47,7 +47,7 @@ describe('K_Means', function() {
 
         data_generate();
 
-        var KMeans = new K_Means({
+        let KMeans = new K_Means({
             random_Init_Count: 4,
             cluster_count: 7,
             max_iterations: 10000,
@@ -56,7 +56,7 @@ describe('K_Means', function() {
         });
 
 
-        var spy1 = sinon.spy(KMeans, "initiate_Centroids"),
+        let spy1 = sinon.spy(KMeans, "initiate_Centroids"),
             spy2 = sinon.spy(KMeans, "assign_Clusters"),
             spy3 = sinon.spy(KMeans, "update_Centroids"),
             spy4 = sinon.spy(KMeans, "apply_Callback");
@@ -67,7 +67,7 @@ describe('K_Means', function() {
 
             it('Should return diagnostic information after completing clustering.', function(done) {
 
-                var success = false;
+                let success = false;
 
                 if (return_Val[0].hasOwnProperty('iteration') && return_Val[0].hasOwnProperty('clusters') && return_Val[0].hasOwnProperty('centroids') && return_Val[0].hasOwnProperty('clustering_complete')) {
                     if (return_Val[0]['clustering_complete'] === true)
@@ -79,7 +79,7 @@ describe('K_Means', function() {
             });
 
             it('Should call initiate_Centroids method.', function(done) {
-                var success = true;
+                let success = true;
                 if (!spy1.called && spy1.callCount === 1)
                     success = false;
                 spy1.restore();
@@ -89,7 +89,7 @@ describe('K_Means', function() {
             });
 
             it('Should call assign_Clusters method.', function(done) {
-                var success = true;
+                let success = true;
                 if (!spy2.called && spy2.callCount === return_Val[0].iteration)
                     success = false;
                 spy2.restore();
@@ -98,7 +98,7 @@ describe('K_Means', function() {
             });
 
             it('Should call update_Centroids method.', function(done) {
-                var success = true;
+                let success = true;
                 if (!spy3.called && spy3.callCount === return_Val[0].iteration)
                     success = false;
                 spy3.restore();
@@ -107,7 +107,7 @@ describe('K_Means', function() {
             });
 
             it('Should apply callback function.', function(done) {
-                var success = true;
+                let success = true;
                 if (!spy4.called && spy4.callCount === (return_Val[0].iteration / 10))
                     success = false;
                 spy4.restore();
@@ -122,11 +122,11 @@ describe('K_Means', function() {
 
 
             it('Should produce unique random number.', function() {
-                var hmp = {},
+                let hmp = {},
                     stub = 0,
                     val = true;
-                for (var i = 0; i < 10; i++) {
-                    var rndv = KMeans.unique_Random_Val(mathjs.matrix(data));
+                for (let i = 0; i < 10; i++) {
+                    let rndv = KMeans.unique_Random_Val(mathjs.matrix(data));
                     if (hmp[rndv] === true) {
                         val = false;
                         break;
@@ -138,8 +138,8 @@ describe('K_Means', function() {
             });
 
             it('Should generate the correct number of centroids specified with the right dimension.', function() {
-                var centroids = mathjs.matrix(KMeans.initiate_Centroids(mathjs.matrix(data)));
-                var success = true;
+                let centroids = mathjs.matrix(KMeans.initiate_Centroids(mathjs.matrix(data)));
+                let success = true;
                 if (Number(centroids.size()[0]) !== 7 || Number(centroids.size()[1]) !== 5) {
 
                     success = false;
@@ -153,17 +153,17 @@ describe('K_Means', function() {
 
         describe("While assigning clusters.", function() {
 
-            var spy5 = sinon.spy(KMeans, "distance");
+            let spy5 = sinon.spy(KMeans, "distance");
 
             it('Should create clusters with the right dimensions.', function() {
-                var clusters = (KMeans.assign_Clusters(mathjs.matrix(data)));
-                var success = true;
+                let clusters = (KMeans.assign_Clusters(mathjs.matrix(data)));
+                let success = true;
                 if (Number(clusters.length) !== 7) {
 
                     success = false;
                 }
 
-                for (var i = 0; i < clusters.length; i++) {
+                for (let i = 0; i < clusters.length; i++) {
                     if (Number(clusters[i].length) === 0) {
 
                         success = false;
@@ -176,7 +176,7 @@ describe('K_Means', function() {
             });
 
             it('Should call distance method.', function(done) {
-                var success = true;
+                let success = true;
                 if (!spy5.called)
                     success = false;
                 spy5.restore();
@@ -186,14 +186,14 @@ describe('K_Means', function() {
 
             it('Should return the distance matrix with the correct values and dimensions.', function() {
 
-                var ref_difference = [];
-                var u = 76.98;
-                var success = true;
-                var difference = [];
-                var input = mathjs.matrix(data);
-                var diff_square_sum = [];
+                let ref_difference = [];
+                let u = 76.98;
+                let success = true;
+                let difference = [];
+                let input = mathjs.matrix(data);
+                let diff_square_sum = [];
 
-                for (var i = 0; i < input.size()[0]; i++) {
+                for (let i = 0; i < input.size()[0]; i++) {
                     diff_square_sum[i] = mathjs.sum(mathjs.square(mathjs.subtract(u, input._data[i]))); // ||u-X||^2  = (u1-X1)^2+....+(un-Xn)^2
                 }
 
@@ -201,7 +201,7 @@ describe('K_Means', function() {
 
                 ref_difference = KMeans.distance(input, u);
 
-                for (var j = 0; j < difference.length; j++) {
+                for (let j = 0; j < difference.length; j++) {
                     if (difference[j] !== ref_difference[j]) {
                         success = false;
                         break;
@@ -219,8 +219,8 @@ describe('K_Means', function() {
 
         describe("While updating centroids.", function() {
             it("Should return the updated centroids with the right dimensions.", function() {
-                var success = true;
-                var centroids = mathjs.matrix(KMeans.update_Centroids());
+                let success = true;
+                let centroids = mathjs.matrix(KMeans.update_Centroids());
                 if (centroids.size()[0] !== 7 && centroids.size()[1] !== 5) {
                     success = false;
                 }
